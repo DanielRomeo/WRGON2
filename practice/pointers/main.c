cbf8c8c170002404ef3edba1f23e67684d2440ee
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-12-06 16:01:07 +0200

    Started the practice dir of the application

---
+++ practice/pointers/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+	// declare a pointer:
+	int* point = NULL; 
+	int year;
+
+	printf("What year is it?");
+	scanf("%d", &year);
+
+	point = &year;
+
+	printf("\nThe year is %d", year); // print the actual number
+	printf("\n The year's address is %p", &year); // print te address of the pointer
+	printf("\n The point's address is %p", &point); // this prints out the pointers address
+
+	printf("\nHello\n");
+	return 0;
+}
